e0394e2e053ff5b3f81c3844b7152c21e5b4ce7f
7xr <55927013+7xrrr@users.noreply.github.com>
2025-10-20 13:26:21 +0100

    Fixed an issue where play activity tasks were not being solved correctly.
    Updated the play activity task handling, it no longer requires joining a user’s voice channel or accessing their account.
    Now, the Discord API accepts a simple key like call:${user.id}:1
    which allows triggering the activity without needing an actual voice session.

--- src/quests/PLAY_ACTIVITY.ts
+++ src/quests/PLAY_ACTIVITY.ts
@@ -6,24 +6,25 @@ import ms from "ms";
 
 export default new QuestConfig({
     name: "PLAY_ACTIVITY",
-    requireLogin: true,
-    requireVoiceChannel: true,
+    requireLogin: false,
+    requireVoiceChannel: false,
     async run(user: ChildUser) {
 
         const secondsNeeded = user.target;
         let progress = user.current || 0;
-        const streamKey = `call:${questsConfig.voice.channel}:1`
+        const streamKey = `call:${user.id}:1`
         while (!user.stoped) {
             const heartbeat = await user.api
                 .post(`quests/${user.quest}/heartbeat`, {
                     stream_key: streamKey,
                     terminal: false
                 }).catch((err) => err.response);
-
+                console.log(heartbeat?.data)
             if (!heartbeat?.data?.user_id) {
                 user.stop("Error sending heartbeat");
                 break;
             }
+        
 
             const response = user.extractProgress(heartbeat.data);
 
